cafd1278ea2289163f4f850b4e4f770db9df8bd4
egmen <egmen@mail.ru>
2017-08-23 22:54:43 +0300

    [+] Тесты и базовый JS основных функций.

--- public/index.js
+++ public/index.js
@@ -1,3 +1,30 @@
 (function main () {
-  console.log('Started!')
+  class MyForm {
+    constructor () {
+      this.fio = document.querySelector('input[name="fio"]')
+      this.email = document.querySelector('input[name="email"]')
+      this.phone = document.querySelector('input[name="phone"]')
+    }
+    validate () {
+      return {}
+    }
+    getData () {
+      return {
+        fio: this.fio.value,
+        email: this.email.value,
+        phone: this.phone.value
+      }
+    }
+    setData ({fio, email, phone}) {
+      this.fio.value = fio
+      this.email.value = email
+      this.phone.value = phone
+      return
+    }
+    submit () {
+      return
+    }
+  }
+
+  window.MyForm = new MyForm()
 })()

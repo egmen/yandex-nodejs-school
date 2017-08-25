a70261ba2b7591f8b8e67bcd910e122016fec552
egmen <egmen@mail.ru>
2017-08-25 21:17:07 +0300

    [+] MVP!!!
    [+] Генератор случайного результата.
    [+] Функция установки резултата и класса.

--- public/index.js
+++ public/index.js
@@ -78,8 +78,60 @@
      * @return {Boolean}      Отмена стандартного действия, если JS загрузился
      */
     function submit (form) {
-      form.submit()
+      const {
+        action,
+        method
+      } = form
+      const body = new window.FormData(form)
+      const fakeAction = createFakeAction()
+      const options = fakeAction
+        ? {}
+        : {method, body}
+
+      window.fetch(fakeAction || action, options)
+        .then((result) => result.json())
+        .then((result) => {
+          const {status, reason, timeout} = result
+
+          switch (status) {
+            case 'progress':
+              setResultState(status)
+              window.setTimeout(() => submit(form), timeout)
+              break
+            case 'success':
+              setResultState(status)
+              break
+            case 'error':
+              setResultState(status, reason)
+              break
+            default:
+              throw new Error('Неизвестный статус')
+          }
+        })
+        .catch((err) => {
+          setResultState('error', `Ошибка получения ответа ${err.toString()}`)
+        })
+
+      // Для отмены отправки формы средствами браузера
       return false
+    }
+
+    /**
+     * Получение случайного названия файла по заложенной вероятности
+     * @return {String} Имя файла, которое в ответ получит форма
+     */
+    function createFakeAction () {
+      const actionTypes = {
+        success: 2,
+        error: 1,
+        progress: 10
+      }
+      const actionsList = Object
+        .keys(actionTypes)
+        .reduce((list, curr) => list.concat(Array(actionTypes[curr]).fill(curr)), [])
+      const randomActionKey = Math.floor(Math.random() * actionsList.length)
+
+      return `response/${actionsList[randomActionKey]}.json`
     }
 
     /**
@@ -98,12 +150,30 @@
     /**
      * Установка класса ошибки полю ввода
      * при несоответвии наличия класса и валидности поля (XOR), сделать toggle
-     * @param {Element}  input   Поле ввода HTML
+     * @param {Element}  inputField   Поле ввода HTML
      */
-    function setErrorClass (input, isValid) {
-      if (input.classList.contains(errorClassName) ^ !isValid) {
-        input.classList.toggle(errorClassName)
+    function setErrorClass (inputField, isValid) {
+      if (inputField.classList.contains(errorClassName) ^ !isValid) {
+        inputField.classList.toggle(errorClassName)
       }
+    }
+
+    /**
+     * Установка класса результата
+     * @param {String} className Наименование класса для установки
+     * @param {String} text      Текст для установки в компоненте
+     */
+    function setResultState (className, text = '') {
+      const classList = 'progress_success_error'.split('_')
+      const resultContainer = document.querySelector('#resultContainer')
+
+      // Проверка соответсвия текущего класса и отсутвию других
+      classList.forEach((currentClass) => {
+        if (resultContainer.classList.contains(currentClass) ^ !(className === currentClass)) {
+          resultContainer.classList.toggle(errorClassName)
+        }
+      })
+      resultContainer.innerHTML = text
     }
   }
 
